1b1345caf0a2a2a77c21ee7e0643c155ba5ecd72
Daniel Marbach <daniel.marbach@openplace.net>
2024-08-10 13:40:33 +0200

    Use docker on linux to speed up builds (#38)
    * Use docker on linux to speed up builds
    
    * Improve cleanup

--- setup.ps1
+++ setup.ps1
@@ -9,51 +9,76 @@ param (
     [string]$registryPass
 )
 
-$hostInfo = curl -H Metadata:true "169.254.169.254/metadata/instance?api-version=2017-08-01" | ConvertFrom-Json
-$region = $hostInfo.compute.location
-$runnerOsTag = "RunnerOS=$($Env:RUNNER_OS)"
-$packageTag = "Package=$tagName"
+$dockerImage = "rabbitmq:$imageTag"
+$runnerOs = $Env:RUNNER_OS ?? "Linux"
+$resourceGroup = $Env:RESOURCE_GROUP_OVERRIDE ?? "GitHubActions-RG"
+$ipAddress = "127.0.0.1"
 
-echo "hostname=$hostname" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf-8 -Append
-echo "Creating RabbitMQ container $hostname in $region (This can take a while.)"
+if ($runnerOs -eq "Linux") {
+    Write-Output "Running Rabbit in container $($containerName) using Docker"
 
-$azureContainerCreate = "az container create --image rabbitmq:$imageTag --name $hostname --location $region --dns-name-label $hostname --resource-group GitHubActions-RG --cpu 4 --memory 16 --ports 5672 15672 --ip-address public"
+    docker run --name "$($hostname)" -d -p "5672:5672" -p "15672:15672" $dockerImage
+}
+elseif ($runnerOs -eq "Windows") {
 
-if ($registryUser -and $registryPass) {
-    echo "Creating container with login to $registryLoginServer"
-    $azureContainerCreate =  "$azureContainerCreate --registry-login-server $registryLoginServer --registry-username $registryUser --registry-password $registryPass"
-} else {
-    echo "Creating container with anonymous credentials"
+    if ($Env:REGION_OVERRIDE) {
+        $region = $Env:REGION_OVERRIDE
+    }
+    else {
+        $hostInfo = curl -H Metadata:true "169.254.169.254/metadata/instance?api-version=2017-08-01" | ConvertFrom-Json
+        $region = $hostInfo.compute.location
+    }
+
+    $runnerOsTag = "RunnerOS=$($Env:RUNNER_OS)"
+    $packageTag = "Package=$tagName"
+    $dateTag = "Created=$(Get-Date -Format "yyyy-MM-dd")"
+
+    $azureContainerCreate = "az container create --image $dockerImage --name $hostname --location $region --dns-name-label $hostname --resource-group $resourceGroup --cpu 4 --memory 16 --ports 5672 15672 --ip-address public"
+
+    if ($registryUser -and $registryPass) {
+        Write-Output "Creating container with login to $registryLoginServer"
+        $azureContainerCreate =  "$azureContainerCreate --registry-login-server $registryLoginServer --registry-username $registryUser --registry-password $registryPass"
+    } else {
+        Write-Output "Creating container with anonymous credentials"
+    }
+
+    Write-Output "Creating RabbitMQ container $hostname in $region (This can take a while.)"
+
+    $jsonResult = Invoke-Expression $azureContainerCreate
+    if (!$jsonResult) {
+        Write-Output "Failed to create RabbitMQ container"
+        exit 1;
+    }
+
+    $details = $jsonResult | ConvertFrom-Json
+
+    if (!$details.ipAddress) {
+        Write-Output "Failed to create RabbitMQ container $hostname in $region"
+        Write-Output $jsonResult
+        exit 1;
+    }
+
+    $ipAddress=$details.ipAddress.ip
+
+    Write-Output "::add-mask::$ipAddress"
+    Write-Output "Tagging container image"
+    az tag create --resource-id $details.id --tags $packageTag $runnerOsTag $dateTag | Out-Null
+
+}
+else {
+    Write-Output "$runnerOs not supported"
+    exit 1
 }
 
-$jsonResult = Invoke-Expression $azureContainerCreate
-if (!$jsonResult) {
-    Write-Output "Failed to create RabbitMQ container"
-    exit 1;
-}
-
-$details = $jsonResult | ConvertFrom-Json
-
-if (!$details.ipAddress) {
-    Write-Output "Failed to create RabbitMQ container $hostname in $region"
-    Write-Output $jsonResult
-    exit 1;
-}
-
-$ip=$details.ipAddress.ip
-
-echo "::add-mask::$ip"
-echo "Tagging container image"
-
-$dateTag = "Created=$(Get-Date -Format "yyyy-MM-dd")"
-$ignore = az tag create --resource-id $details.id --tags $packageTag $runnerOsTag $dateTag
-
-echo "$connectionStringName=host=$ip" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
+Write-Output "hostname=$hostname" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf-8 -Append
+Write-Output "$connectionStringName=host=$ipAddress" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
 if (-not [string]::IsNullOrWhiteSpace($hostEnvVarName)) {
-    echo "$hostEnvVarName=$ip" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
+    Write-Output "$hostEnvVarName=$ipAddress" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
 }
 
-$uri = "http://" + $ip + ":15672/api/health/checks/virtual-hosts"
+Write-Output "::group::Testing connection"
+
+$uri = "http://" + $ipAddress + ":15672/api/health/checks/virtual-hosts"
 $tries = 0
 
 do {
@@ -69,3 +94,4 @@ if ($response.status -ne "ok") {
     Write-Output "Failed to connect after 50 attempts";
     exit 1
 }
+Write-Output "::endgroup::"
